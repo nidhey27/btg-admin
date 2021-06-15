7b66d59d88488862c3926cb8f9ddd7b07717f46f
ritwikchavhan <ritwikchavhan19@gmail.com>
2021-06-15 13:08:04 +0530

    News Testimonials Map

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: "https://btg-app.herokuapp.com/"
-  apiUrl: 'http://192.168.29.121:3000/'
+  apiUrl: "https://btg-app.herokuapp.com/"
+  // apiUrl: 'http://192.168.29.121:3000/'
 };
 
 /*
